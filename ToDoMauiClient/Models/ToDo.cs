8eb6eb955f8a26452b8bbe1f0064cc009b4f7f7c
SphepheloN <SphepheloN@ZAZN-BET-5E9AAC>
2024-02-14 11:00:51 +0200

    #Models 1. Add a 'Models' folder on the project 2. Add a 'ToDo.cs' class 2.1. Make the class 'public' not 'internal' 2.2. Inherit and implement the 'INotifyPropertyChanged' interface 2.3. Models implementation

---
+++ ToDoMauiClient/Models/ToDo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoMauiClient.Models
+{
+    public class ToDo : INotifyPropertyChanged
+    {
+        int _id;
+        public int Id 
+        {
+            get => _id;
+            set
+            {
+                if (_id == value)
+                    return;
+
+                _id = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Id)));
+            } 
+        }
+
+        string _toDoName;
+        public string ToDoName
+        {
+            get => _toDoName;
+            set
+            {
+                if (_toDoName == value)
+                    return;
+
+                _toDoName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ToDoName)));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
