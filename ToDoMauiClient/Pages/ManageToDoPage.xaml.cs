36bc6ab1a5bb8253b8d7cf14c869bc07a408c7c3
SphepheloN <SphepheloN@ZAZN-BET-5E9AAC>
2024-02-21 12:48:21 +0200

    #Implementing 'ManageToDoPage' 1. 'xaml.cs' 1.1. Add constructors 1.2. Add the following methods --- 'IsNew' --- 'OnSaveButtonClicked' --- 'OnDeleteButtonClicked' --- 'OnCancelButtonClicked'

--- ToDoMauiClient/Pages/ManageToDoPage.xaml.cs
+++ ToDoMauiClient/Pages/ManageToDoPage.xaml.cs
@@ -1,9 +1,65 @@
+using System.Diagnostics;
+using ToDoMauiClient.DataServices;
+using ToDoMauiClient.Models;
+
 namespace ToDoMauiClient.Pages;
 
+[QueryProperty(nameof(ToDo), "ToDo")]
 public partial class ManageToDoPage : ContentPage
 {
-	public ManageToDoPage()
+	private readonly IRestDataService _dataService;
+	ToDo _toDo;
+	bool _isNew;
+
+	public ToDo ToDo
+	{
+		get => _toDo;
+		set
+		{
+			_isNew = IsNew(value);
+			_toDo = value;
+			OnPropertyChanged();
+		}
+	}
+	public ManageToDoPage(IRestDataService dataService)
 	{
 		InitializeComponent();
+
+		_dataService = dataService;
+		BindingContext = this;
+	}
+
+	bool IsNew(ToDo toDo)
+	{
+        if (toDo.Id == 0)
+        {
+			return true;
+        }
+		return false;
+    }
+
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+	{
+		if (_isNew)
+		{
+			Debug.WriteLine("---> Add New Item");
+			await _dataService.AddToDoAsync(ToDo);
+		}
+		else
+		{
+            Debug.WriteLine("---> Updating Item");
+            await _dataService.UpdateToDoAsync(ToDo);
+        }
+	}
+
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+	{
+		await _dataService.DeleteToDoAsync(ToDo.Id);
+        await Shell.Current.GoToAsync("...");
+    }
+
+    async void OnCancelButtonClicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("...");
 	}
 }
